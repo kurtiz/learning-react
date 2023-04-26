0554812ed9cca9d4c0d46e2e5f900e7548234c59
kurtiz <papiliocurtis@gmail.com>
2023-04-26 15:43:10 +0000

    added props :hammer:

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import ListGroup from './components/ListGroup';
 
+let items = [
+    'New York', 'San Francisco',
+    'Tokyo', 'London',
+    'Paris'
+];
+
+const handleSelectItem = (item: string) => {
+    console.log(item);
+}
 
 function App() {
-    return <div><ListGroup /></div>
+    return (
+        <div>
+            <ListGroup
+                items={items}
+                heading="Cities"
+                onSelectItem={handleSelectItem}
+            />
+        </div>)
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,20 +1,34 @@
-function ListGroup() {
-    let items = [
-        'New York', 'San Francisco',
-        'Tokyo', 'London',
-        'Paris'
-    ];
+import {MouseEvent, useState} from "react";
+
+// Props for ListGroup Components
+interface ListGroupProps {
+    items: string[],
+    heading: string,
+    onSelectItem: (item: string) => void
+}
+
+function ListGroup({items, heading, onSelectItem }: ListGroupProps) {
+
+
+    const [selectedIndex, setSelectedIndex] = useState(-1);
 
     return (
         <>
-            <h1>List</h1>
+            <h1>{heading}</h1>
             { items.length === 0 && <p>No items</p> }
             <ul className="list-group">
                 {items.map((item, index) => (
                     <li
                         key={item}
-                        className="list-group-item"
-                        onClick={()=>console.log(item, index)}
+                        className={
+                            selectedIndex === index ?
+                                "list-group-item active" :
+                                "list-group-item"}
+                        onClick={ () => {
+                            setSelectedIndex(index);
+                            onSelectItem(item);
+                        }
+                    }
                     >
                         {item}
                     </li>
